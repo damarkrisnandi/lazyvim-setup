187aaf38121ab1ddcba73cd17a6d42c322d1ea45
damar <damar@gitlab.lab>
2024-02-05 13:46:57 +0700

    add toggleterm ^& dadbodui

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,5 +3,10 @@
 -- Add any additional keymaps here
 --
 -- Git stuff
+vim.keymap.set("n", "<leader>ght", ":Gitsigns toggle_current_line_blame<CR>", { desc = "Toggle Current Line Blame" })
 vim.keymap.set("n", "<leader>ghB", ":Git blame<CR>", { desc = "Fugitive Blame" })
 
+-- toggleterm
+vim.keymap.set("n", "<leader>f1", ":ToggleTerm 1<CR>", { desc = "Toggle Terminal 1" })
+vim.keymap.set("n", "<leader>f2", ":ToggleTerm 2<CR>", { desc = "Toggle Terminal 2" })
+vim.keymap.set("n", "<leader>f3", ":ToggleTerm 3<CR>", { desc = "Toggle Terminal 3" })
